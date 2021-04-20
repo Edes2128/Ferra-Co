f5869e7b54fc11dc9896820795bb0d505941f8a1
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-04-20 17:33:21 +0200

    ASAS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,28 @@
+const menuIcon = document.querySelector('.menu-icon');
+const sideMenu = document.querySelector('.side-menu');
+const sideMenuOpa = document.querySelector('.side-menu-opa');
+const body = document.querySelector('body');
+const closeSideMenu = document.querySelector('.close-side-menu');
+
+menuIcon.addEventListener('click', function(){
+      sideMenu.classList.add('side-menu-open')
+      sideMenuOpa.classList.add('side-menu-open')
+      body.style.overflow = 'hidden'
+})
+
+sideMenuOpa.addEventListener('click', function(){
+  sideMenu.classList.remove('side-menu-open')
+  sideMenuOpa.classList.remove('side-menu-open')
+  body.style.overflow = 'unset'
+})
+
+closeSideMenu.addEventListener('click' ,function(){
+  sideMenu.classList.remove('side-menu-open')
+  sideMenuOpa.classList.remove('side-menu-open')
+  body.style.overflow = 'unset'
+})
+
+
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 let currentScrollPos = window.pageYOffset;

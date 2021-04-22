6602210c5f93fb8094282292efeffc0d20bbbf89
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-04-22 13:14:48 +0200

    ASAS

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,20 +3,31 @@ const sideMenu = document.querySelector('.side-menu');
 const sideMenuOpa = document.querySelector('.side-menu-opa');
 const body = document.querySelector('body');
 const closeSideMenu = document.querySelector('.close-side-menu');
+const loadingSpinner = document.querySelector('.loading');
 
-menuIcon.addEventListener('click', function(){
-      sideMenu.classList.add('side-menu-open')
-      sideMenuOpa.classList.add('side-menu-open')
-      body.style.overflow = 'hidden'
+window.addEventListener('load', () => {
+  body.style.overflow = 'hidden'
+  setTimeout(showPage, 1000);
 })
 
-sideMenuOpa.addEventListener('click', function(){
+function showPage() {
+  loadingSpinner.style.display = "none";
+  body.style.overflow = 'unset'
+}
+
+menuIcon.addEventListener('click', function () {
+  sideMenu.classList.add('side-menu-open')
+  sideMenuOpa.classList.add('side-menu-open')
+  body.style.overflow = 'hidden'
+})
+
+sideMenuOpa.addEventListener('click', function () {
   sideMenu.classList.remove('side-menu-open')
   sideMenuOpa.classList.remove('side-menu-open')
   body.style.overflow = 'unset'
 })
 
-closeSideMenu.addEventListener('click' ,function(){
+closeSideMenu.addEventListener('click', function () {
   sideMenu.classList.remove('side-menu-open')
   sideMenuOpa.classList.remove('side-menu-open')
   body.style.overflow = 'unset'
@@ -24,12 +35,12 @@ closeSideMenu.addEventListener('click' ,function(){
 
 
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-let currentScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".header").style.top = "0";
-  }else if(currentScrollPos <= 0){
-      document.querySelector(".header").style.top = "0";
+  } else if (currentScrollPos <= 0) {
+    document.querySelector(".header").style.top = "0";
   } else {
     document.querySelector(".header").style.top = "-200px";
   }
